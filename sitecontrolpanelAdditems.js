92366ee3b0e018bb50cd57c869a2984941f1645e
pkprogramer61346134 <137704275+pkprogramer61346134@users.noreply.github.com>
2023-11-14 13:40:15 +0530

    updata_two

--- sitecontrolpanelAdditems.js
+++ sitecontrolpanelAdditems.js
@@ -1,4 +1,4 @@
-function Addtimes(gets){
+function Addtimes(){
 
            
     var getbutton = document.getElementById('submat');
@@ -11,7 +11,7 @@ function Addtimes(gets){
         var brand = document.getElementsByName("brand")[0];
         var Model = document.getElementsByName("Model")[0];
        
-             if(gets == "" ){
+            
           obj[0].command = "ADD";
           obj[0].filedata[0].catogry = catogry.value;
           obj[0].filedata[0].brand =  brand.value;
@@ -19,20 +19,61 @@ function Addtimes(gets){
           obj[0].filedata[0].price = price.value;
           obj[0].filedata[0].Name = name.value;
           obj[0].filedata[0].model_no = Model.value;
-        }
+      
 
 
          Get_from_Server().then(response =>{
 
           
-          if(gets != "" ){
-              
-          }
+              console.log(response);
 
          } )
    
      });
   
+
+
+}
+
+function  eits()
+{
+  const params = new URLSearchParams(window.location.search);
+  const argumentValue = params.get('argument');
+
+  Get_from_Server().then(response =>{
+
+    for (let i = 0; i < Response.message.length; i++) {
+
+              if(Response.message[i].Items_id == argumentValue)
+              {
+
+                var catogry = document.getElementsByName("Catogry")[0];
+                var name = document.getElementsByName("Name")[0];
+                var detail = document.getElementsByName("Detail")[0];
+                var price = document.getElementsByName("price")[0];
+                var brand = document.getElementsByName("brand")[0];
+                var Model = document.getElementsByName("Model")[0];
+               
+                    
+                  obj[0].command = "ADD";
+                catogry.textContent = obj[0].filedata[0].catogry ;
+                brand.textContent  =obj[0].filedata[0].brand ;
+                detail.textContent = obj[0].filedata[0].detail ;
+                 price.textContent =obj[0].filedata[0].price ;
+                name.textContent = obj[0].filedata[0].Name ;
+                Model.textContent  =obj[0].filedata[0].model_no ;
+
+                         
+
+              }
+     
+ 
+
+    }
+
+
+
+} ) 
 
 
 }
